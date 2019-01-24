790cdb687eaf7f0a2f0fd5783dd74df0f712ce5f
Justin Stonebarger <jjstonebarger@gmail.com>
2019-01-24 03:33:48 -0600

    intital commit

---
+++ src/main/java/org/launchcode/salessamurai/SalessamuraiApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.salessamurai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalessamuraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalessamuraiApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/org/launchcode/salessamurai/controllers/HelloSamurai.java
@@ -0,0 +1,16 @@
+package org.launchcode.salessamurai.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloSamurai {
+
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index() {
+        return "Hello Samurai";
+    }
+}
