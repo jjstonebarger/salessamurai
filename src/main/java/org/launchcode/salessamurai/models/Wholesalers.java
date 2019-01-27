9530670d06bd43a829568cc298876421d8c70f47
Justin Stonebarger <jjstonebarger@gmail.com>
2019-01-27 03:11:30 -0600

    Build v1.8

--- src/main/java/org/launchcode/salessamurai/models/Wholesalers.java
+++ src/main/java/org/launchcode/salessamurai/models/Wholesalers.java
@@ -1,4 +1,22 @@
 package org.launchcode.salessamurai.models;
 
 public enum Wholesalers {
+
+    SPORTSSOUTH ("Sportssouth"),
+    RSRGROUP ("RSRGroup"),
+    GREENSUPPLY ("GreenSupply"),
+    DAVIDSONS ("Davidsons");
+
+    private final String name;
+
+    Wholesalers(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
+
+
