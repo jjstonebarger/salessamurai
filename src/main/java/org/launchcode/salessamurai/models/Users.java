5be69e28a7979c8408c3295635b492970985fd1d
Justin Stonebarger <jjstonebarger@gmail.com>
2019-01-27 03:01:16 -0600

    Build v1.7

--- src/main/java/org/launchcode/salessamurai/models/Users.java
+++ src/main/java/org/launchcode/salessamurai/models/Users.java
@@ -1,4 +1,65 @@
 package org.launchcode.salessamurai.models;
 
+
+import java.util.List;
+
+
+
+@Entity
 public class Users {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=5, max=15)
+    private String username;
+
+    @Email
+    private String email;
+
+    @NotNull
+    @Size(min=6)
+    private String password;
+
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    private List<Product> products;
+
+    public Users() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Product> getProducts() {return products;}
+
 }
+
