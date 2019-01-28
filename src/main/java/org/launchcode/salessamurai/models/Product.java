2a3d0ea9d8a54c08afea5423897e5afff02711dd
Justin Stonebarger <jjstonebarger@gmail.com>
2019-01-28 01:16:45 -0600

    Build v1.26

--- src/main/java/org/launchcode/salessamurai/models/Product.java
+++ src/main/java/org/launchcode/salessamurai/models/Product.java
@@ -1,8 +1,12 @@
 package org.launchcode.salessamurai.models;
 
 
-
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Product {
